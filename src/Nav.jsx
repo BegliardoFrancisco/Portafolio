e9f2e0879587d8ddd9a583223986474b1a6a9f4b
BegliardoFrancisco <panchobegliardo@gmail.com>
2024-12-04 16:17:00 -0300

    presentacion maquetada

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,18 @@
 import './styles/App.css'
-
+import home from './assets/house-solid.svg'
+import proyects from './assets/code-solid.svg'
 function Nav() {
 
   return (
     <nav>
       <ul>
-        <li><a href="">Proyectos</a></li>
-        <li><a href="">Inicio</a></li>
+        <li><a href=""><img className='icon' src={proyects}/></a></li>
+        <li><a href=""><img className='icon' src={home}/></a></li>
       </ul>
+      <div className='container_foto'>
+        <img className='perfil' src='' alt="" />
+      </div>
+      
     </nav>
   )
 }
