601e989c8e4b0be50b57aab64ccdecd3ffd2b3fd
BegliardoFrancisco <panchobegliardo@gmail.com>
2024-12-05 00:30:25 -0300

    'presentation'

---
+++ src/Presentation.jsx
@@ -0,0 +1,16 @@
+import './styles/presentation.css'
+
+function Presentation() {
+
+
+    return (
+        <>
+            <div className="container-presentacion">
+                <h1 className="name">Francisco Begliardo</h1>
+                <h2>Full Stack Development</h2>
+            </div>
+        </>
+    )
+}
+
+export default Presentation
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import Nav from './Nav'
+import Presentaion from './Presentation'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
       <Nav></Nav>
+      <Presentaion></Presentaion>
     </>
   </StrictMode>,
 )
